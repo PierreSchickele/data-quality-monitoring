fc14ec3e34c03054d056dfc4b51bafaad6c8832a
pierre <pierre.schickele@live.fr>
2025-02-01 15:50:33 +0100

    connect to api example

--- extract_data/get_api_data.py
+++ extract_data/get_api_data.py
@@ -1,7 +1,9 @@
 import sys
 from datetime import date
+import requests
 
-if __name__ == "__main__":
+
+def read_date_input() -> None:
     if len(sys.argv) > 1:
         try:
             year, month, day = [int(v) for v in sys.argv[1].split("-")]
@@ -11,3 +13,18 @@ if __name__ == "__main__":
             print("Format incorrect. Saisir une date de format : YYYY-MM-DD")
     else:
         print("Indiquer une date en paramètre")
+
+
+def connect_api() -> None:
+    url = "https://catfact.ninja/fact"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        print(data["fact"])
+    else:
+        print("Erreur lors de la requête :", response.status_code)
+
+
+if __name__ == "__main__":
+    connect_api()
