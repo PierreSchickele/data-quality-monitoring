9d79a8e941660fbc951377d9cca6bee6856ceeeb
pierre <pierre.schickele@live.fr>
2025-02-01 16:26:22 +0100

    connect to local API and documentation

--- extract_data/get_api_data.py
+++ extract_data/get_api_data.py
@@ -21,16 +21,34 @@ def read_date_input() -> date | None:
         print("Indiquer une date en paramètre")
 
 
-def connect_api() -> None:
-    url = "https://catfact.ninja/fact"
+def connect_api(store_name: str, queried_date: date, sensor_id: int | None) -> None:
+    """
+    Connect to the API, and print the JSON output
+    :param store_name: name of the store
+    :param queried_date: date of the observation
+    :param sensor_id: id of the sensor (optional)
+    :return: no output
+    """
+    year, month, day = queried_date.year, queried_date.month, queried_date.day
+
+    endpoint = "http://127.0.0.1:8000"
+    url = endpoint + f"?store_name={store_name}&year={year}&month={month}&day={day}"
+
+    if sensor_id is not None:
+        url += f"&sensor_id={sensor_id}"
+
     response = requests.get(url)
 
     if response.status_code == 200:
         data = response.json()
-        print(data["fact"])
+        print(data)
     else:
         print("Erreur lors de la requête :", response.status_code)
 
 
 if __name__ == "__main__":
-    connect_api()
+    date_input = read_date_input()
+    store = "Lille"
+
+    if date_input is not None:
+        connect_api(store_name=store, queried_date=date_input, sensor_id=None)
