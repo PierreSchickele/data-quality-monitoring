6657e26490b2659cc0056bb60814a14c1df1f276
pierre <pierre.schickele@live.fr>
2025-02-01 16:25:40 +0100

    documentation read_date_input

--- extract_data/get_api_data.py
+++ extract_data/get_api_data.py
@@ -3,12 +3,18 @@ from datetime import date
 import requests
 
 
-def read_date_input() -> None:
+def read_date_input() -> date | None:
+    """
+    Verify that the parameter given by the user is a date in YYYY-MM-DD format,
+    then return the date.
+    :return: The date in YYYY-MM-DD format
+    """
     if len(sys.argv) > 1:
         try:
             year, month, day = [int(v) for v in sys.argv[1].split("-")]
             queried_date = date(year, month, day)
             print(queried_date)
+            return queried_date
         except ValueError:
             print("Format incorrect. Saisir une date de format : YYYY-MM-DD")
     else:
